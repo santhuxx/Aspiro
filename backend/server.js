698a99ac82bf42de771d051e75d37a752a7365fd
PoornimaWeerasekara <it22608468@my.sliit.lk>
2025-05-15 04:41:27 +0530

    Complete task funtion
    Complete task funtion

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const authRoutes = require('./routes/auth');
+const taskRoutes = require('./routes/tasks');
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+app.use('/api/tasks', taskRoutes);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
